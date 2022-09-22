d86da6b25a910da2b8433e57306c5aa1d4fd2534
mohamed gamal <69708798+GEMY74@users.noreply.github.com>
2022-09-22 15:51:02 +0200

    Add files via upload

---
+++ async await/app.js
@@ -0,0 +1,29 @@
+let url = "https://api.npoint.io/46461ce47b29357c1b51";
+
+async function fetchUser() {
+  let response = await fetch(url);
+  let users = await response.json();
+
+  // Format the array of users by adding an extra full_name attribute to each item
+  users.forEach((user) => {
+    user.full_name = `${user.firstName} ${user.lastName} `;
+  });
+  console.log(users);
+
+  // Get only males who are older than 30
+  users.forEach((user) => {
+    if (user.age > 30 && user.gender == "male") {
+      console.log(user);
+    }
+  });
+
+  // Group the filtered users by nationality : {EG:[{ .... },{ .... }]};
+  result = users.reduce(function (acc, obj) {
+    acc[obj.nat] = acc[obj.nat] || [];
+    acc[obj.nat].push(obj);
+    return acc;
+  },{});
+  console.log(result);
+}
+
+fetchUser();
