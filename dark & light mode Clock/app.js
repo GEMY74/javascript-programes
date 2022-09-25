a4c124552df89ea7ca810e2b94c87f15b4f391cb
mohamed gamal <69708798+GEMY74@users.noreply.github.com>
2022-09-25 14:51:40 +0200

    Add files via upload

---
+++ dark & light mode Clock/app.js
@@ -0,0 +1,60 @@
+// selecting html elements
+let hourLine = document.querySelector(".hour");
+let minuteLine = document.querySelector(".minute");
+let secondLine = document.querySelector(".second");
+let dateDiv = document.querySelector(".date");
+
+// day & month array
+let days = ["Sun", "Mon", "Tue", "Wed", "Thur", "Fri", "Sat"];
+let months = [
+  "Jan",
+  "Feb",
+  "Mar",
+  "Apr",
+  "May",
+  "Jun",
+  "Jul",
+  "Aug",
+  "Sep",
+  "Oct",
+  "Nov",
+  "Dec",
+];
+
+// function to get the current time
+function setTime() {
+  let time = new Date();
+  let hours = time.getHours();
+  let minutes = time.getMinutes();
+  let seconds = time.getSeconds();
+  let day = time.getDay();
+  let date = time.getDate();
+  let month = time.getMonth();
+  let year = time.getFullYear();
+
+  // the rotation of circle clock
+  hourLine.style.transform = ` rotate(${setRotate(hours, 12)}deg)`;
+  minuteLine.style.transform = `rotate(${setRotate(minutes, 60)}deg)`;
+  secondLine.style.transform = ` rotate(${setRotate(seconds, 60)}deg)`;
+  // setting the date
+  dateDiv.innerHTML = `${days[day]}, ${months[month]} ${date} ${year}`;
+}
+
+// function to set the rotation of each line
+let setRotate = (time, num) => {
+  return (time * 360) / num;
+};
+
+// call the function every time the page is load
+setTime();
+
+// call the function every 1second
+setInterval(setTime, 1000);
+
+// ====== toggle between light and dark mode ====
+const toggle = document.getElementById("toggle");
+const container = document.querySelector(".container");
+
+toggle.addEventListener("change", () => {
+  container.classList.toggle("light");
+});
